4e892158b58bec91c2afcb203762828fc30932cb
paulparas <paul.paraspaul@gmail.com>
2017-04-30 23:41:00 +0530

    Align Dev Env Initial Setup

---
+++ Scripts/Controllers/app.js
@@ -0,0 +1,112 @@
+﻿
+var alignApp = angular.module('alignApp', ['jcs-autoValidate', 'ngSanitize', 'ngRoute', 'ngAnimate', 'ui.mask', 'ui.bootstrap', 'ngFileUpload', 'ui.calendar', 'ngDialog']);
+
+alignApp.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "Login.html",
+        controller: "loginController"
+
+    })
+        
+    .when("/SignUp", {
+        templateUrl: "SignUp.html",
+        controller: "signUpController"
+    })
+
+    .when("/Home", {
+        templateUrl: "Home.html",
+        controller: "homeController"
+    })
+    ;
+    $locationProvider.html5Mode({
+        enabled: false,        
+        requireBase: false,
+
+    })
+    $locationProvider.hashPrefix('');
+});
+    
+
+//configuration required for jcs auto validate
+alignApp.run([
+        'bootstrap3ElementModifier',
+        function (bootstrap3ElementModifier) {
+            bootstrap3ElementModifier.enableValidationStateIcons(true);
+        }
+
+]);
+
+alignApp.controller("pageLoadController", function ($scope, $location, $rootScope) {
+    $scope.pageLoadComplete = false;
+    $scope.headerCenter = true;
+    angular.element(function () {
+        $scope.pageLoadComplete = true;
+        
+    });
+
+    $scope.path = $location.path();
+    //console.log($scope.path);
+
+    $scope.$on('adjustHeader', function (event, data) {
+        if(data==1)
+        {
+            $scope.headerCenter = false;
+        }
+        else {
+            $scope.headerCenter = true;
+        }
+    });
+
+
+        //if ($scope.path == '/SignUp' || $scope.path == '/') {
+        //    $scope.headerCenter = true;
+        //}
+        //else {
+            
+        //}
+
+   
+});
+
+function ConfirmPasswordValidatorDirective(defaultErrorMessageResolver) {
+    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+        errorMessages['confirmPassword'] = 'Please ensure the passwords match.';
+    });
+
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        scope: {
+            confirmPassword: '=confirmPassword'
+        },
+        link: function (scope, element, attributes, ngModel) {
+            ngModel.$validators.confirmPassword = function (modelValue) {
+                return modelValue === scope.confirmPassword;
+            };
+
+            scope.$watch('confirmPassword', function () {
+                ngModel.$validate();
+            });
+        }
+    };
+}
+
+ConfirmPasswordValidatorDirective.$inject = [
+  'defaultErrorMessageResolver'
+];
+
+alignApp.directive('confirmPassword', ConfirmPasswordValidatorDirective);
+
+//alignApp.run(function ($rootScope, $location) {
+
+//    // To maintain the route on page refresh
+//    if ($location.$$path != "") sessionStorage.routeChange = false;
+
+//    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+//        if ($rootScope.rootEmpId == null) {
+//            //    $location.path("/login");
+//        }
+//    });
+//})
+
---
+++ Scripts/Controllers/homeController.js
@@ -0,0 +1,213 @@
+﻿alignApp.controller('homeController', function ($scope, defaultErrorMessageResolver, $timeout, ngDialog) {
+
+
+    $scope.$emit('adjustHeader', 1);
+    //define scope variables
+    $scope.initialise = function () {
+        $scope.signUpInfo1 = true;
+        $scope.menuStatus = [
+            { isOpen: true },
+            { isOpen: false },
+        ];
+        $scope.leftNavId = 0    ;
+        $scope.showAll = true;
+        defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+            errorMessages['maxSize'] = 'File too large. Maximum allowed size is 2MB.';
+            errorMessages['time'] = 'Please enter a valid time.';
+            errorMessages['hours'] = 'Invalid hours.';
+            errorMessages['minutes'] = 'Invalid minutes.';
+        });
+        $scope.renderCalender = function (calendar) {
+            if ($scope.uiConfig.calendars[calendar]) {
+                console.log('.', $scope.uiConfig.calendars[calendar]);
+                $scope.uiConfig.calendars[calendar].fullCalendar('render');
+            }
+        };
+
+        $timeout(function () {
+            //$scope.renderCalender('eventsCalender');
+        }, 1000);
+        
+
+    }
+
+
+    $scope.setNavId = function (id) {
+        $scope.leftNavId = id;
+    }    
+
+    //initialise scope variables
+    $scope.initialise();    
+
+    $scope.toggleMenuStatus = function (menuId) {
+        angular.forEach($scope.menuStatus, function (value, key) {
+            if (key != menuId)
+            value.isOpen = false;
+        });
+
+        if ($scope.menuStatus[menuId].isOpen) {
+            $scope.menuStatus[menuId].isOpen = false;
+        }
+        else {
+            $scope.menuStatus[menuId].isOpen = true;
+        }
+    }
+
+
+    $scope.setSignUpInfo = function (flag) {
+        switch (flag) {
+            case 1:
+                $scope.signUpInfo1 = true;
+                $scope.class = "ng-hide-remove1";
+                break;
+            case 2:
+                $scope.signUpInfo1 = false;
+                $scope.class = "ng-hide-remove2";
+        }
+
+    }
+
+    //function to be called on form submit
+    $scope.submitLoginForm = function () {
+
+
+
+    }
+
+    $scope.alertEventOnClick = function () {
+        ngDialog.open({ template: '_PartialViews/EventInfo.html', className: 'ngdialog-theme-default' });
+    }
+    //-----------------------MyEvents--------------------------------------//
+    $scope.uiConfig = {
+        calendar: {
+            height: 700,
+            editable: true,
+            header: {
+                left: 'month basicWeek basicDay',
+                center: 'title',
+                right: 'today prev,next'
+            },
+            eventClick: $scope.alertEventOnClick,
+            eventDrop: $scope.alertOnDrop,
+            eventResize: $scope.alertOnResize
+        }
+    };
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    /* event source that contains custom events on the scope */
+    $scope.events = [
+      { title: 'CocaHeads Meeting', start: new Date(y, m, 5,20,0) },
+      { title: 'CocaHeads Meeting', start: new Date(y, m, 10, 20, 0) },
+      { title: 'CocaHeads Meeting', start: new Date(y, m, 18, 20, 0) },
+      { title: 'CocaHeads Meeting', start: new Date(y, m, 28, 20, 0) },
+      { title: 'Techincally Philly Meeting', start: new Date(y, m, 13, 20, 0) },      
+    ];
+
+    $scope.eventSources = [$scope.events];
+
+    $('.left-menu-nav').on('click', function () {
+        if ($('#leftNavBtn').is(":visible"))
+        {
+            $('#leftNavBtn').click();
+        }
+        
+    });
+
+    $scope.dateOptions = {
+        dateDisabled: false,
+        formatYear: 'yy',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(),
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.dt = new Date();
+    
+
+    $scope.popup1 = {
+        opened: false
+    };
+
+    $scope.openDatePicker = function () {
+        $scope.popup1.opened = true;
+    };
+
+    $scope.$watch('leftNavId', function () {
+        if($scope.leftNavId==2)
+        {
+            $timeout(function () {
+                disableVisualStylingTime();
+            }, 100);
+            
+        }
+    })
+
+    var disableVisualStylingTime = function()
+    {
+        $('#time :input').attr('disable-validation-message', '');
+        $('#time :input').attr('disable-invalid-styling', 'true');
+        $('#time :input').attr('disable-valid-styling', 'true');
+        $('#date').attr('disable-validation-message', '');
+    }
+    
+});
+
+
+
+function ReviewsRatingsDirective() {
+    return {
+        restrict: 'A',
+        scope: {
+            review: '@review',
+            rating: '=rating',
+            name: '@name',
+            designation: '@designation',
+            imgurl: '@imgurl',
+        },
+        templateUrl: '_PartialViews/ReviewTemplate.html',
+        link: function (scope, element, attributes) {
+
+        }
+    };
+};
+
+function PastEventsDirective() {
+    return {
+        restrict: 'A',
+        scope: {
+            eventSubTitle: '@eventSubTitle',
+            eventDateTime: '@eventDateTime',
+            eventPlace: '@eventPlace',
+            eventPlaceAddress: '@eventPlaceAddress',
+            eventImgUrl: '@eventImgUrl',
+        },
+        templateUrl: '_PartialViews/PastEventTemplate.html',
+        link: function (scope, element, attributes) {
+
+        }
+    }
+}
+
+function RangeFilter() {
+    return function(arr,range)
+    {
+        range = parseInt(range);
+        for(var i=0;i<range;i++)
+        {
+            arr.push(i);
+        }
+        return arr;
+    }
+};
+
+alignApp.directive('reviewsRatings', ReviewsRatingsDirective);
+
+alignApp.directive('pastEvents', PastEventsDirective);
+
+alignApp.filter('range', RangeFilter);
