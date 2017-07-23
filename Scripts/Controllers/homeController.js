53a662d1ae374b6aa94b417060404b8d56299d58
paulparas <paul.paraspaul@gmail.com>
2017-07-23 12:15:28 +0530

    Comments added

--- Scripts/Controllers/homeController.js
+++ Scripts/Controllers/homeController.js
@@ -1,26 +1,83 @@
 ﻿alignApp.controller('homeController', function ($rootScope, $scope,$stateParams, $http, defaultErrorMessageResolver, $timeout, ngDialog, ngProgressFactory, $compile, $state, uiCalendarConfig, $transitions,Upload) {
 
-    $scope.progressbar = ngProgressFactory.createInstance();
-    $scope.progressbar.start();
-    $scope.form = {};
-    $scope.evtForm = {};
-    $scope.evtForm.evtId = 0;
-    $scope.evtForm.evtImgUrl = "";
-    $scope.obj = {};
-    $scope.obj.time = $scope.obj.date = new Date();
-    $scope.evtBtnDisabled = false;
-    $scope.eventSources = [];
-    $scope.events = [];
-    $scope.category = {};
 
-    $scope.createBtnText = "Create";
-    
-    $scope.invalid = false;
-    $scope.selectedList = [];
-    $transitions.onStart({}, function (trans) {
+    //---------------------------initialise scope variables------------------------------------//
+    $scope.initialiseCtrl = function () {
+        $scope.progressbar = ngProgressFactory.createInstance();
         $scope.progressbar.start();
-    });
+        $scope.form = {};
+        $scope.evtForm = {};
+        $scope.evtForm.evtId = 0;
+        $scope.evtForm.evtImgUrl = "";
+        $scope.obj = {};
+        $scope.obj.time = $scope.obj.date = new Date();
+        $scope.evtBtnDisabled = false;
+        $scope.eventSources = [];
+        $scope.events = [];
+        $scope.category = {};
+        $scope.invalid = false;
+        $scope.createBtnText = "Create";
+        $scope.selectedList = [];
+    }
 
+    $scope.initialiseObjects = function () {
+        $scope.navigation = {
+            "home": [
+            {
+                "id": 1,
+                "name": "Live",
+                "tile": "live"
+            },
+            {
+                "id": 2,
+                "name": "Event+",
+                "tile": "event"
+            },
+            {
+                "id": 3,
+                "name": "My Events",
+                "tile": "myevents"
+            },
+             {
+                 "id": 4,
+                 "name": "Reviews/ Ratings",
+                 "tile": "reviews"
+             },
+            {
+                "id": 5,
+                "name": "Past Events",
+                "tile": "pastevents"
+            },
+            ]
+        };
+
+        $scope.categoryList = [
+      { id: 1, name: "Meetup" },
+      { id: 2, name: "Party" },
+      { id: 3, name: "Conference" },
+       { id: 4, name: "Golf Match" },
+      { id: 5, name: "Lunch" },
+      { id: 6, name: "Dinner" }
+        ];
+
+        $scope.dateOptions = {
+            dateDisabled: false,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+        $scope.altInputFormats = ['M!/d!/yyyy'];
+    }
+
+    $scope.initialiseCtrl();
+    $scope.initialiseObjects();   
+    
+
+    //---------------------------------validations-------------------------------------//
     $scope.$watch('evtForm.evtCategory', function (newValue, oldValue) {
         if ($scope.form.createEvtForm != undefined) {
             if ($scope.form.createEvtForm.multipleSelect.$touched) {
@@ -59,9 +116,21 @@
         }
     }
 
+    var disableVisualStylingTime = function () {
+        $('#time :input').attr('disable-validation-message', '');
+        $('#time :input').attr('disable-invalid-styling', 'true');
+        $('#time :input').attr('disable-valid-styling', 'true');
+        $('#date').attr('disable-validation-message', '');
+    }
+
+    var validateTime = function () {
+        $scope.datetimeChanged('time');
+    }
+
+    //---------------------------Add A New Event Category Type-------------------------------//
     $scope.addCategory = function () {
         var categoryExists = false;        
-        if ($scope.category.name != undefined)
+        if ($scope.category.name != undefined && $scope.category.name!="")
         {
             Object.keys($scope.categoryList).forEach(function (key) {
                 if ($scope.categoryList[key].name == $scope.category.name) {
@@ -75,10 +144,10 @@
                 $scope.evtForm.evtCategory.push($scope.category);
                 $scope.category = {};
             }            
-        }
-            
+        }            
     }
 
+    //-----------------------------function to upload picture to server-----------------------//
     $scope.uploadPic = function (file) {       
         $scope.progressbar.start();
         $scope.evtBtnDisabled = true;
@@ -122,72 +191,8 @@
         //    data: { evtData: angular.toJson($scope.evtForm), evtPicture: file },
         //});
     }
-   
 
-    $rootScope.$on('$viewContentLoaded', function () {        
-        var route = $state.$current.name;        
-        route = route.substr(5, route.length);
-        for (var i = 0; i < $scope.navigation.home.length; i++)
-        {
-            if($scope.navigation.home[i].tile==route)
-            {
-                $scope.leftNavId = $scope.navigation.home[i].id;                
-                break;
-            }
-            else
-            {
-                $scope.leftNavId = 0;                                
-            }
-        }
-        if ($scope.leftNavId == 0) {
-            $state.go("home.dashboard");
-        }        
-        $timeout(function () {
-            $scope.endProgress();
-        }, 0);        
-    });
-
-
-    $scope.navigation = {
-        "home":[ 
-        {
-            "id": 1,
-            "name":"Live",
-            "tile": "live"
-        },
-        {
-            "id": 2,
-            "name": "Event+",
-            "tile": "event"
-        },
-        {
-            "id": 3,
-            "name": "My Events",
-            "tile": "myevents"
-        },
-         {
-             "id": 4,
-             "name": "Reviews/ Ratings",
-             "tile": "reviews"
-         },
-        {
-            "id": 5,
-            "name": "Past Events",
-            "tile": "pastevents"
-        },
-         ]      
-    }
-
-
-    $scope.categoryList = [
-  { id: 1, name: "Meetup" },
-  { id: 2, name: "Party" },
-  { id: 3, name: "Conference" },
-   { id: 4, name: "Golf Match" },
-  { id: 5, name: "Lunch" },
-  { id: 6, name: "Dinner" }
-    ];
-
+    //------------------------------function to get Json Data for Align-------------------------------------------//
     $scope.getJsonData = function () {
         //$http.get('../StaticDataFiles/StaticJsonData.json').then(function (response) {
         $http.get('https://raw.githubusercontent.com/off2on/Align2k17Dev/master/StaticJsonData.json').then(function (response) {
@@ -201,26 +206,56 @@
             }
         })
     }
+    //----------------------------function to get Event Data as Json-----------------------------------------------//
+    $scope.getEventData = function () {
+        //$http.get('../StaticDataFiles/EventData.json').then(function (response) {
+        $http.get('https://raw.githubusercontent.com/off2on/Align2k17Dev/master/StaticDataFiles/EventData.json').then(function (response) {
+            $scope.evtForm = response.data;
+            var momentDate = moment($scope.evtForm.evtDt + ' ' + $scope.evtForm.evtTime);
+            $scope.obj.date = $scope.obj.time = momentDate.toDate();
 
-    $scope.setCalendar = function () {       
+            $scope.obj.picture = $scope.evtForm.evtImgUrl;
+        })
+    }
+
+    $scope.getEventData();
+    $scope.getJsonData();
+
+    //----------------------------Event Calender Initialization And Related Events and Functions-------------------------------//
+
+    $scope.eventRender = function (event, element, view) {
+        element.attr({
+            'uib-tooltip': event.title,
+            'tooltip-append-to-body': true
+        });
+        $compile(element)($scope);
+    };
+
+    $scope.alertEventOnClick = function (date) {
+
+        $scope.selectedEvent = date;
+
+        ngDialog.open({ template: '_PartialViews/EventInfo.html', className: 'ngdialog-theme-default', scope: $scope });
+    }
+
+    $scope.dt = new Date();
+
+    $scope.popup1 = {
+        opened: false
+    };
+
+    $scope.openDatePicker = function () {
+        $scope.popup1.opened = true;
+    };
+
+    $scope.setCalendar = function () {
         refreshCalendar($scope.jsonData.MyEvents);
     }
 
     function refreshCalendar(events) {        
         uiCalendarConfig.calendars.calendar.fullCalendar('removeEvents');
         uiCalendarConfig.calendars.calendar.fullCalendar('addEventSource', events);
-    }
-
-    $scope.getEventData = function () {
-        //$http.get('../StaticDataFiles/EventData.json').then(function (response) {
-        $http.get('https://raw.githubusercontent.com/off2on/Align2k17Dev/master/StaticDataFiles/EventData.json').then(function (response) {
-            $scope.evtForm = response.data;
-            var momentDate = moment($scope.evtForm.evtDt +' '+ $scope.evtForm.evtTime);
-            $scope.obj.date = $scope.obj.time = momentDate.toDate();
-            
-            $scope.obj.picture = $scope.evtForm.evtImgUrl;
-        })
-    }
+    }    
 
     $scope.initialiseCalender = function (myEvents) {
         //$http.get('../StaticDataFiles/MyEventsData.json').then(function (response) {
@@ -250,46 +285,11 @@
 
                 }
             };
-        
     }
-
-    $scope.getEventData();
-    $scope.getJsonData();
     
-
-
-    $scope.getCategoriesTextRaw = function (item) {
-        //return '@' + item.name;
-        //console.log(item.label);
-        if($scope.selectedCategories.indexOf(item)==-1)
-            $scope.selectedCategories.push(item);
-        else
-
-        return '+' + item.label;
-    };
-
-
-    var disableVisualStylingTime = function () {
-        $('#time :input').attr('disable-validation-message', '');
-        $('#time :input').attr('disable-invalid-styling', 'true');
-        $('#time :input').attr('disable-valid-styling', 'true');
-        $('#date').attr('disable-validation-message', '');
-    }
-
-    angular.element(function () {
-        $scope.progressbar.complete();
-            $scope.$watch('leftNavId', function () {
-        if ($scope.leftNavId == 2) {
-            $timeout(function () {
-                disableVisualStylingTime();
-            }, 500);
-
-        }
-    });
-    });
-    $scope.$emit('adjustHeader', 1);
-    //define scope variables
-    $scope.initialise = function () {
+    
+    //----------------------------Initialize Scope Functions-------------------------------------------------------//
+    $scope.initialiseFunctions = function () {
         $scope.signUpInfo1 = true;
         $scope.menuStatus = [
             { isOpen: true },
@@ -316,34 +316,30 @@
             
             $scope.progressbar.start();
         }
-        
 
         $scope.endProgress = function ($event) {
-            
             $scope.progressbar.complete();
-            
-            //$event.preventDefault();
         }
-
-
     }
 
+    $scope.initialiseFunctions();
+
+    //------------------------------------Function to be called when clicked on edit existing event----------------------//
     $scope.editEvent = function (id) {        
         $state.go('home.event');
         ngDialog.close();
     }
 
+    //-----------------------------------Function to set selected id of left menu navigation-----------------------//
     $scope.setLeftNavId = function (id) {
         if ($scope.leftNavId != id)
         {
             //$scope.startProgress($event);
             $scope.leftNavId = id;            
         }               
-    }    
+    }
 
-    //initialise scope variables
-    $scope.initialise();    
-
+    //------------------------------Function to toggle accordian on contact section----------------------------------//
     $scope.toggleMenuStatus = function (menuId) {
         angular.forEach($scope.menuStatus, function (value, key) {
             if (key != menuId)
@@ -358,7 +354,7 @@
         }
     }
 
-
+    //----------------------------Function to apply slide animation on dashboard tiles--------------------------------//
     $scope.setSignUpInfo = function (flag) {
         switch (flag) {
             case 1:
@@ -369,14 +365,9 @@
                 $scope.signUpInfo1 = false;
                 $scope.class = "ng-hide-remove2";
         }
-
     }
 
-    var validateTime = function () {
-        $scope.datetimeChanged('time');        
-    }
-
-    //function to be called on form submit
+    //------------------function to be called on event form submit----------------------------------//
     $scope.submitEvtForm = function () {
         console.log($scope.inputValue);
         validateTime();
@@ -385,76 +376,52 @@
         if (validateCategory(categories)) {
             IsFormValid = $scope.form.createEvtForm.$valid;
         }
-        
         if (IsFormValid)
         {            
             $scope.evtForm.evtTime = $scope.obj.time.toLocaleTimeString();
             $scope.evtForm.evtDt = $scope.obj.date.toDateString();
             $scope.uploadPic($scope.obj.picture);
         }
-        
-        
-        //if(obj.$invalid==true)
-        //{
-        //    $scope.invalid = true;
-        //    $('.ng-ms').addClass('event-has-error');
-        //    $('.ng-ms').removeClass('event-has-success');
-        //}
-        //else {
-        //    $('.ng-ms').removeClass('event-has-error');
-        //    $('.ng-ms').addClass('event-has-success');
-        //}
-
-        //console.log($scope.evtForm);
-        
-
     }
 
-    //-----------------------MyEvents--------------------------------------//
- 
+    //---------------------------------Events to run on DOM ready---------------------------------------//
+    $transitions.onStart({}, function (trans) {
+        $scope.progressbar.start();
+    });
 
-    $scope.eventRender = function (event, element, view) {
-        element.attr({
-            'uib-tooltip': event.title,
-            'tooltip-append-to-body': true
+    $rootScope.$on('$viewContentLoaded', function () {
+        var route = $state.$current.name;
+        route = route.substr(5, route.length);
+        for (var i = 0; i < $scope.navigation.home.length; i++) {
+            if ($scope.navigation.home[i].tile == route) {
+                $scope.leftNavId = $scope.navigation.home[i].id;
+                break;
+            }
+            else {
+                $scope.leftNavId = 0;
+            }
+        }
+        if ($scope.leftNavId == 0) {
+            $state.go("home.dashboard");
+        }
+        $timeout(function () {
+            $scope.endProgress();
+        }, 0);
+    });
+
+    angular.element(function () {
+        $scope.progressbar.complete();
+        $scope.$watch('leftNavId', function () {
+            if ($scope.leftNavId == 2) {
+                $timeout(function () {
+                    disableVisualStylingTime();
+                }, 500);
+
+            }
         });
-        $compile(element)($scope);
-    };
-
-    $scope.alertEventOnClick = function (date) {
-
-        $scope.selectedEvent = date;
-
-        ngDialog.open({ template: '_PartialViews/EventInfo.html', className: 'ngdialog-theme-default', scope: $scope });
-    }
-
-    $scope.dateOptions = {
-        dateDisabled: false,
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-    };
-
-    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-    $scope.format = $scope.formats[0];
-    $scope.altInputFormats = ['M!/d!/yyyy'];
-
-    $scope.dt = new Date();
-    
-
-    $scope.popup1 = {
-        opened: false
-    };
-
-    $scope.openDatePicker = function () {
-        $scope.popup1.opened = true;
-    };    
+    });
     
 });
-
-
-
 //--------------------Directives-------------------------------------------------//
 
 function NavigationDirective() {
@@ -604,7 +571,7 @@ function EventReviewsRatingsDirective() {
     }
 }
 
-
+//-------------------Filters-------------------------------------------------//
 function RangeFilter() {
     return function(arr,range)
     {
@@ -617,7 +584,7 @@ function RangeFilter() {
     }
 };
 
-//--------------------directives----------------------------------//
+//--------------------Register Directives----------------------------------//
 
 alignApp.directive('companyInfo', CompanyInfoDirective);
 
@@ -635,7 +602,7 @@ alignApp.directive('liveAll', LiveAllDirective);
 
 alignApp.directive('liveAlign', LiveAlignDirective);
 
-//----------------------filters-----------------------------------//
+//----------------------Regitser filters-----------------------------------//
 
 alignApp.filter('range', RangeFilter);
 
